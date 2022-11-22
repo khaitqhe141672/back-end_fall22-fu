e454e5c6b2e4a3a8655424eae12f9b2cd27e6c52
khaitqhe141672 <65528932+khaitqhe141672@users.noreply.github.com>
2022-11-23 02:27:12 +0700

    update api insert-post-voucher

--- homesharing_back-end/src/main/java/com/homesharing_backend/data/repository/ComplaintPostRepository.java
+++ homesharing_back-end/src/main/java/com/homesharing_backend/data/repository/ComplaintPostRepository.java
@@ -1,6 +1,8 @@
 package com.homesharing_backend.data.repository;
 
 import com.homesharing_backend.data.entity.ComplaintPost;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import org.springframework.stereotype.Repository;
 public interface ComplaintPostRepository extends JpaRepository<ComplaintPost, Long> {
 
     ComplaintPost getComplaintPostById(Long id);
+
+    Page<ComplaintPost> getComplaintPostByHost_Id(Long hostID, PageRequest pageRequest);
 }
--- homesharing_back-end/src/main/java/com/homesharing_backend/service/impl/PostVoucherServiceImpl.java
+++ homesharing_back-end/src/main/java/com/homesharing_backend/service/impl/PostVoucherServiceImpl.java
@@ -3,17 +3,24 @@ package com.homesharing_backend.service.impl;
 import com.homesharing_backend.data.dto.PostVoucherDto;
 import com.homesharing_backend.data.entity.Post;
 import com.homesharing_backend.data.entity.PostVoucher;
+import com.homesharing_backend.data.entity.Voucher;
 import com.homesharing_backend.data.repository.PostRepository;
 import com.homesharing_backend.data.repository.PostVoucherRepository;
+import com.homesharing_backend.data.repository.VoucherRepository;
 import com.homesharing_backend.exception.EmptyDataException;
 import com.homesharing_backend.exception.NotFoundException;
 import com.homesharing_backend.presentation.payload.JwtResponse;
+import com.homesharing_backend.presentation.payload.MessageResponse;
+import com.homesharing_backend.presentation.payload.request.PostVoucherRequest;
 import com.homesharing_backend.service.PostVoucherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -26,6 +33,9 @@ public class PostVoucherServiceImpl implements PostVoucherService {
 
     @Autowired
     private PostRepository postRepository;
+
+    @Autowired
+    private VoucherRepository voucherRepository;
 
     @Override
     public ResponseEntity<JwtResponse> getPostVoucherByPostID(Long postID) {
@@ -60,6 +70,46 @@ public class PostVoucherServiceImpl implements PostVoucherService {
             } else {
                 return ResponseEntity.status(HttpStatus.OK).body(new JwtResponse(HttpStatus.OK.name(), postVoucherDtoList));
             }
+        }
+    }
+
+    @Override
+    public ResponseEntity<MessageResponse> insertPostVoucher(Long postID, PostVoucherRequest postVoucherRequest) {
+
+        Post post = postRepository.getPostById(postID);
+
+        if (Objects.isNull(post)) {
+            throw new NotFoundException("Post-id khong ton tai");
+        } else {
+
+            LocalDateTime localDateTime = LocalDateTime.now();
+            LocalDate localDate = localDateTime.toLocalDate();
+
+            Date dateNow = Date.valueOf(localDate);
+
+            postVoucherRequest.getVoucherIDList().forEach(v -> {
+
+                Voucher voucher = voucherRepository.getVoucherById(v);
+
+                PostVoucher postVoucher = postVoucherRepository.getPostVoucherByPost_IdAndVoucher_Id(post.getId(), voucher.getId());
+
+                if (Objects.isNull(postVoucher)) {
+                    PostVoucher pv = PostVoucher.builder()
+                            .startDate(dateNow)
+                            .endDate(Date.valueOf(localDate.plusDays(voucher.getDueDay())))
+                            .status(1)
+                            .post(post)
+                            .build();
+                    postVoucherRepository.save(pv);
+                } else {
+                    postVoucher.setStartDate(dateNow);
+                    postVoucher.setStatus(1);
+                    postVoucher.setEndDate(Date.valueOf(localDate.plusDays(voucher.getDueDay())));
+                    postVoucherRepository.save(postVoucher);
+                }
+
+            });
+            return ResponseEntity.status(HttpStatus.OK).body(new MessageResponse(HttpStatus.OK.value(), "Add voucher thanh cong"));
         }
     }
 }
