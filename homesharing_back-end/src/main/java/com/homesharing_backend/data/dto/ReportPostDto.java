bc51bea087f6a3b59dfd0e5d74edcd82ad7214a6
khaitqhe141672 <65528932+khaitqhe141672@users.noreply.github.com>
2022-11-23 21:40:15 +0700

    fix api list-reportPost-admin

--- homesharing_back-end/src/main/java/com/homesharing_backend/data/dto/ReportPostDto.java
+++ homesharing_back-end/src/main/java/com/homesharing_backend/data/dto/ReportPostDto.java
@@ -1,10 +1,13 @@
 package com.homesharing_backend.data.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.homesharing_backend.data.entity.ReportType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @AllArgsConstructor
@@ -24,6 +27,8 @@ public class ReportPostDto {
     private int statusPost;
 
     private int statusReportPost;
+
+    private List<ReportType> reportTypes;
 
     public ReportPostDto(Long postID, String title,
                          float price, String imagePostUrl,
