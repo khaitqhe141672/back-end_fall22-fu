5b16916d66042134a60ebc28a0fd85868016e5ea
khaitqhe141672 <65528932+khaitqhe141672@users.noreply.github.com>
2022-12-09 05:02:51 +0700

    fix api

--- homesharing_back-end/src/main/java/com/homesharing_backend/data/repository/BookingRepository.java
+++ homesharing_back-end/src/main/java/com/homesharing_backend/data/repository/BookingRepository.java
@@ -1,6 +1,8 @@
 package com.homesharing_backend.data.repository;
 
 import com.homesharing_backend.data.entity.Booking;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,7 +13,7 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
 
     Booking getBookingById(Long id);
 
-    List<Booking> getBookingByCustomer_Id(Long customerID);
+    Page<Booking> getBookingByCustomer_Id(Long customerID, PageRequest pageRequest);
 
     int countBookingByStatus(int status);
 
