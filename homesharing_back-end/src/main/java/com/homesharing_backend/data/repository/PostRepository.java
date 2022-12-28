0874e160bbc95feedfd167b06b6d3fa6682b04b2
khaitqhe141672 <65528932+khaitqhe141672@users.noreply.github.com>
2022-12-29 00:39:44 +0700

    Update PostRepository.java

--- homesharing_back-end/src/main/java/com/homesharing_backend/data/repository/PostRepository.java
+++ homesharing_back-end/src/main/java/com/homesharing_backend/data/repository/PostRepository.java
@@ -113,7 +113,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     @Query(value = "SELECT new com.homesharing_backend.data.dto.ViewBookingDto(b.id, bd.id, p.id, p.title, bd.startDate, " +
             "bd.endDate, b.totalMoney, bd.totalPerson, bd.totalService, b.note, b.status) FROM Booking b " +
             "LEFT JOIN BookingDetail bd on b.id = bd.booking.id " +
-            "LEFT JOIN Post p on bd.post.id = p.id WHERE p.host.id= :hostID AND b.status= 2 AND (current_date() between bd.startDate AND bd.endDate)")
+            "LEFT JOIN Post p on bd.post.id = p.id WHERE p.host.id= :hostID AND b.status= 3 AND (current_date() between bd.startDate AND bd.endDate)")
     Page<ViewBookingDto> getAllCurrentBooking(@Param("hostID") Long hostID, PageRequest pageRequest);
 
     @Query(value = "SELECT new com.homesharing_backend.data.dto.SearchDto(p.id, p.title, pd.address, p.price, pi.imageUrl, v.description, " +
